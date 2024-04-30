12dc63079903e28a10635bbd8a022802640d6d68
kennyltr <162782938+kennyltr@users.noreply.github.com>
2024-04-30 14:24:36 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '1q3e5tY?',
+        'PASSWORD': '#password',
         'HOST': 'localhost',
         'PORT': '5432',
     }
